203ad5d18468dea8a086b16b1091d587ad745f01
Marco Munizaga <git@marcopolo.io>
2021-09-03 16:35:38 -0700

    aarch64 support

--- flake.nix
+++ flake.nix
@@ -1,42 +1,45 @@
 {
   description = "Lotus Daemon (filecoin) flake";
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
 
     let
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
 
-    {
-      overlay = import ./overlay.nix;
+      {
+        overlay = import ./overlay.nix;
 
-      defaultPackage = forAllSystems (
-        system: (
-          import nixpkgs {
-            inherit system;
-            overlays = [ self.overlay ];
-          }
-        ).lotus
-      );
-
-      packages = forAllSystems (
-        system: (
-          let
-            pkgs = import nixpkgs {
+        defaultPackage = forAllSystems (
+          system: (
+            import nixpkgs {
               inherit system;
-              overlays = [ self.overlay ];
-            };
-          in
-          {
-            lotus = pkgs.lotus;
-            filcrypto = pkgs.filcrypto;
-            filecoin-ffi = pkgs.filecoin-ffi;
-          }
-        )
-      );
+              overlays = [ self.overlay (import rust-overlay) ];
+            }
+          ).lotus
+        );
 
-      nixosModules.lotus = import ./module.nix;
+        packages = forAllSystems (
+          system: (
+            let
+              pkgs = import nixpkgs {
+                inherit system;
+                overlays = [ self.overlay (import rust-overlay) ];
+              };
+            in
+              {
+                lotus = pkgs.lotus;
+                filcrypto = pkgs.filcrypto;
+                filecoin-ffi = pkgs.filecoin-ffi;
+              }
+          )
+        );
 
-    };
+        nixosModules.lotus = import ./module.nix;
+
+      };
 }
--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,24 @@
 final: prev: {
-  lotusSrc = prev.callPackage ./pkgs/lotus-src.nix { };
-  filcrypto = prev.callPackage ./pkgs/filcrypto.nix { };
-  filecoin-ffi = prev.callPackage ./pkgs/filecoin-ffi.nix { };
-  lotus = prev.callPackage ./pkgs/lotus.nix { };
+  lotusSrc = prev.callPackage ./pkgs/lotus-src.nix {};
+  filcrypto = prev.callPackage ./pkgs/filcrypto.nix {
+    # rustPlatform = prev.makeRustPlatform {
+    #   cargo = final.rust-bin.beta.latest.cargo;
+    #   rustc = final.rust-bin.beta.latest.rustc // {
+    #     meta = final.rust-bin.beta.latest.rustc.meta // {
+    #       platforms = prev.rustc.meta.platforms;
+    #     };
+    #   };
+    # };
+    rustPlatform = prev.makeRustPlatform {
+      cargo = final.rust-bin.nightly.latest.default;
+      rustc = final.rust-bin.nightly.latest.default;
+    };
+    # rustPlatform = prev.makeRustPlatform (
+    #   final.rust-bin.beta.latest // {
+    #     platforms = prev.rustc.meta.platforms;
+    #   }
+    # );
+  };
+  filecoin-ffi = prev.callPackage ./pkgs/filecoin-ffi.nix {};
+  lotus = prev.callPackage ./pkgs/lotus.nix {};
 }
